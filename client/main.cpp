8c141c47ded40fe49cc7ec649bf9ca45e1d0fc80
Kuba <jakub.oboza@gmail.com>
2009-03-06 10:27:00 +0100

    added interfce suggestion

--- client/main.cpp
+++ client/main.cpp
@@ -7,6 +7,10 @@ int main(int argc, char* argv[]){
    // getopt server address to run and provide it to start class
    // also read files list from args or use some dir, maybe *.cpp file selection and push it to Client 
 
+  // USAGE: 
+  // i think good interface would be like this ./client 192.168.0.1:7666 jola.cpp misio.cpp jack.cpp
+  //  ./client <server-addr> <files to compile...>
+
   Client::getInstance()->run("192.168.1.1:6000");
   
   return 0;
